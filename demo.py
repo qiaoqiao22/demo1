d5ed621895da9d6e8614948ecbec36bcace207eb
demo <yqiaoqiaosd@163.com>
2019-04-29 23:50:09 +0800

    demo second commit at 2019/4/29

--- demo.py
+++ demo.py
@@ -10,6 +10,8 @@ print(X_b)
 theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
 print(theta_best)
 
+X_b = np.c_[np.ones((100, 1)), X]
+print(X_b)
 
 X_new = np.array([[0], [2]])
 X_new_b = np.c_[(np.ones((2, 1))), X_new]
